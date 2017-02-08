2f573af6e7f23eee2e57a5d517e8c19c51929652
root <root@defb9cf3ab0e>
2017-02-08 08:41:05 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "toTestMavenbuild_allserv",
+  "homePage" : "Main",
+  "name" : "toTestMavenbuild_allserv",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
